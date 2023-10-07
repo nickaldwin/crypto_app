515ea8ee322dfea4df103a51576ab72cc3f34e6c
nickaldwin <nicklemoncito98@gmail.com>
2023-10-07 19:45:34 +0800

    pushing before brownowt

--- client/web3/contracts/Crowdfunding.sol
+++ client/web3/contracts/Crowdfunding.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
+        /*make a class first*/
 contract Crowdfunfing {
     struct Campaign {
         address: owner;
@@ -13,5 +13,46 @@ contract Crowdfunfing {
         address: [] donators;
         uint256: [] donations;   
     }
-    mapping(uint256)
+     /*campaign[0];*/
+
+    mapping(uint256 => Campaign) public campaigns;
+
+    uint256 public numberOfCampaigns = 0; /*public Variable*/
+
+        /*create three functions*/
+    function createCampaign(address_owner, string memory_title, string memory_description, uint256_target, uint256_deadline, string memory_image) public returns(uint256){
+        Campaign storage campaign = campaigns [numberOfCampaigns];
+        //to check is evrething ok
+
+        require(campaign.deadline < block.timestamp, "the deadline should be a date in the future.");
+        campaign.owner = _owner;
+        campaign.title = _tilte;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.ammountCollected = 0;
+        campaign.image = _image; 
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns - 1
+    }
+
+    function donateCampaign(uint256_id, ) public payable {
+        uint256 amount = msg.value;
+        
+        campaign Storage campaign = campaigns [_id];
+
+        campaign.donatiors.push(msg.senser); //to be continue
+    }
+
+    function getDonators(){
+
+    }
+
+    function getCampaign(){
+
+
+    }
+
 }
