c0c362e2a237adcd0a974ed887a67bfdeda74e2f
nickaldwin <nicklemoncito98@gmail.com>
2023-10-08 11:17:15 +0800

    update .gitignore

--- client/web3/hardhat.config.js
+++ client/web3/hardhat.config.js
@@ -38,7 +38,7 @@ module.exports = {
       hardhat: {},
       goerli: {
         url: 'https://rpc.ankr.com/eth_goerli',
-        accounts: ['0x${process.env.PRIVATE_KEY}'],
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
       }
     },
     settings: {
